96d9fef5e4c3b83bca17250f896d07e8485b4caf
AliGadraoui <ali.gadraoui03@gmail.com>
2025-01-06 17:49:28 +0200

    Fixed reducer - incrementing GOOD correctly

--- part6/unicafe/src/reducer.js
+++ part6/unicafe/src/reducer.js
@@ -8,16 +8,16 @@ const counterReducer = (state = initialState, action) => {
   console.log(action)
   switch (action.type) {
     case 'GOOD':
-      return state
+      return { ...state, good: state.good + 1 }  // Increment 'good'
     case 'OK':
-      return state
+      return { ...state, ok: state.ok + 1 }      // Increment 'ok'
     case 'BAD':
-      return state
+      return { ...state, bad: state.bad + 1 }    // Increment 'bad'
     case 'ZERO':
+      return initialState                        // Reset state
+    default:
       return state
-    default: return state
   }
-  
 }
 
 export default counterReducer
